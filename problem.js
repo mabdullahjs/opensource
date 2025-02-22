fe78cacbc053b01dfd39340ee2cb8f1497488228
Azam-web778 <asifanadeem482@gmail.com>
2025-02-22 10:22:25 -0800

    logic building problems

---
+++ problem.js
@@ -0,0 +1,53 @@
+console.log("Hello world");
+
+// Problems for logic building
+
+// 1. Sum of two numbers
+
+// function sum(num1 , num2) {
+//     return num1 + num2;
+// }
+// console.log(sum(5 , 5));
+
+// 2. Find largest value
+
+// function findLargestValue(a , b , c) {
+//     if (a > b && a > c) {
+//         console.log("value a is greater ->" , a);
+//     }else if(b > a && b > c){
+//         console.log("value b is greater ->" , b);
+//     }else if(c > a && c > b){
+//         console.log("value c is greater ->" , c);
+//     }else{
+//         console.log("no value is greater");
+//     }
+// }
+// findLargestValue(10 , 5 , 3)
+// findLargestValue(5 , 20 , 5)
+// findLargestValue(5 , -5 , 30)
+
+
+
+
+
+//3. Merge Two Sorted Arrays
+
+
+// function mergeArray(array1 , array2) {
+//     console.log( "meged array ->", [...array1 , ...array2]);
+    
+// }
+// mergeArray([1 ,2 ,3 ,4 ,5] , [6 ,7 , 8, 9, 10])
+
+
+// 4. Remove Duplicates from an Array
+
+
+// function removeDuplicate(arr) {
+//     return [... new Set(arr)]
+// }
+// const fruits = ["orange" , "orange" , "apple" , "mango" , "mango"]
+// console.log(removeDuplicate(fruits));
+
+
+
